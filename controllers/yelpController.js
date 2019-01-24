eabe5c135f3b1bd305df124f72ad71f4d83f2eec
MMPark <parkmee@gmail.com>
2019-01-24 00:26:37 -0500

    moved script for seeding to seedDB

--- controllers/yelpController.js
+++ controllers/yelpController.js
@@ -8,39 +8,9 @@ const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/trek-tips';
 mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
 
 // Yelp endpoint URLs
-const categoriesURL = `https://api.yelp.com/v3/categories`;
 const searchURL = `https://api.yelp.com/v3/businesses/search`;
 
 module.exports = {
-  // run to return categories from Yelp and to populate category collection
-  // only needs to be run one time to populate database
-  getCategories: () => {
-    axios.get(categoriesURL, {
-      headers: {
-        'Authorization': `Bearer ${process.env.YELP_API_KEY}`,
-        'Access-Control-Allow-Headers': 'Origin'
-      }
-    }).then(res => {
-      console.log(res.data.categories);
-      const categories = res.data.categories;
-
-      db.Category
-        .remove({})
-        .then(() => db.Category.collection.insertMany(categories))
-        .then(data => {
-          console.log(data.result.n + " records inserted!");
-          process.exit(0);
-        })
-        .catch(err => {
-          console.log(`Mongoose error: ${err}`);
-          process.exit(1);
-        });
-    })
-      .catch(err => {
-        console.log(`API error: ${err}`);
-      });
-  },
-  
   // use to return recommended places based on location (required) and categories (optional)
   // location search string examples: "New York City", "NYC", "350 5th Ave, New York, NY 10118"
   // to filter for multiple categories: "bars,french" will filter by bars OR french
--- scripts/seedDB.js
+++ scripts/seedDB.js
@@ -1,30 +1,71 @@
+const axios = require("axios");
 const mongoose = require("mongoose");
 const db = require("../models");
 mongoose.set('useCreateIndex', true);
 
 // connect to mongoose database
 const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/trek-tips';
-mongoose.connect(MONGODB_URI, {useNewUrlParser: true});
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
 
-// seed data for Cuisine collection
-const categorySeed = [
-  {alias: "category 1 alias", title: "category 1 title", parent_aliases: [{alias: "restaurants"}]},
-  {alias: "category 2 alias", title: "category 2 title", parent_aliases: [{alias: "active life"}]},
-  {alias: "category 3 alias", title: "category 3 title", parent_aliases: [{alias: "arts & entertainment"}]},
-  {alias: "category 4 alias", title: "category 4 title", parent_aliases: [{alias: "restaurants"}]},
-  {alias: "category 5 alias", title: "category 5 title", parent_aliases: [{alias: "restaurants"}]}
-  // TODO get categories from yelp API: https://www.yelpblog.com/2018/01/yelp_category_list
-];
+// category endpoint url to seed category collection
+const categoriesURL = `https://api.yelp.com/v3/categories`;
 
-// empties, then seeds Category collection
-db.Category
-  .remove({})
-  .then(() => db.Category.collection.insertMany(categorySeed))
-  .then(data => {
-    console.log(data.result.n + " records inserted!");
-    process.exit(0);
-  })
+// business search url and search parameters to seed places collection
+const searchURL = `https://api.yelp.com/v3/businesses/search`;
+const categories = "desserts";
+const location = "atlanta, ga"
+
+// seed place data
+axios.get(searchURL, {
+  params: {
+    categories: categories,
+    location: location,
+    limit: 5
+  },
+  headers: {
+    'Authorization': `Bearer lC3zgwezYWCKbJZW03Yepl4A52o_fhrqd9a1x0_MapVxItu97aAHOUOGfsRzDJswOWzWlaHv0zvw8keaePumFEkXJWyOgcTcLg7ekQOQ9skybUd_wy02lE3hnQy0W3Yx`,
+    'Access-Control-Allow-Headers': 'Origin'
+  }
+}).then(res => {
+  console.log(res.data.businesses);
+  const businesses = res.data.businesses;
+
+  db.Category
+    .remove({})
+    .then(() => db.Place.collection.insertMany(businesses))
+    .then(data => {
+      console.log(data.result.n + " records inserted!");
+    })
+    .catch(err => {
+      console.log(`Mongoose error: ${err}`);
+      process.exit(1);
+    });
+})
   .catch(err => {
-    console.log(err);
-    process.exit(1);
+    console.log(`API error: ${err}`);
+  });
+
+axios.get(categoriesURL, {
+  headers: {
+    'Authorization': `Bearer lC3zgwezYWCKbJZW03Yepl4A52o_fhrqd9a1x0_MapVxItu97aAHOUOGfsRzDJswOWzWlaHv0zvw8keaePumFEkXJWyOgcTcLg7ekQOQ9skybUd_wy02lE3hnQy0W3Yx`,
+    'Access-Control-Allow-Headers': 'Origin'
+  }
+}).then(res => {
+  console.log(res.data.categories);
+  const categories = res.data.categories;
+
+  db.Category
+    .remove({})
+    .then(() => db.Category.collection.insertMany(categories))
+    .then(data => {
+      console.log(data.result.n + " records inserted!");
+      process.exit(0);
+    })
+    .catch(err => {
+      console.log(`Mongoose error: ${err}`);
+      process.exit(1);
+    });
+})
+  .catch(err => {
+    console.log(`API error: ${err}`);
   });
