72bcf739ffb77b5190fabc6b360035de71c3e301
MMPark <parkmee@gmail.com>
2019-01-23 23:57:12 -0500

    resolved merge conflict

--- models/place.js
+++ models/place.js
@@ -1,31 +1,6 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-<<<<<<< HEAD:models/place.js
-=======
-// sub document for Category title
-const Category = new Schema({
-  title: { type: String, required: true }
-});
-
-// sub document for Coordinates
-const Coordinates = new Schema({
-  latitude: { type: Number },
-  longitude: { type: Number }
-});
-
-// sub document for location (address)
-const Location = new Schema({
-  address1: { type: String, required: true },
-  address2: { type: String },
-  address3: { type: String },
-  city: { type: String, required: true },
-  state: { type: String },
-  country: { type: String, required: true },
-  display_address: { type: String }
-});
-
->>>>>>> 5fca8a0ee2f05247cb5e7b7d7a18c35698834792:server/models/place.js
 // main schema for places (via Yelp)
 // refer to https://www.yelp.com/developers/documentation/v3/business for response body
 const PlaceSchema = new Schema({
