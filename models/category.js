e2ae8d9b5b89e66ad70ec5e2e6a04f414f0e98bf
MMPark <parkmee@gmail.com>
2019-02-07 19:33:02 -0500

    updated category schema with image ref id

--- models/category.js
+++ models/category.js
@@ -4,7 +4,8 @@ const Schema = mongoose.Schema;
 const CategorySchema = new Schema({
   alias: { type: String, required: true, unique: true },
   name: { type: String, required: true },
-  parent_aliases: [{ type: String }]
+  parent_aliases: [{ type: String }],
+  image_id: { type: Schema.Types.ObjectId, ref: "Image" }
 });
 
 const Category = mongoose.model("Category", CategorySchema);
