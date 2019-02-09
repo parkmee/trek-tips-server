ea91706c6d4c583dfe272053a0b46bc33859d657
MMPark <parkmee@gmail.com>
2019-02-09 17:15:02 -0500

    renamed 'name' to 'title'

--- models/category.js
+++ models/category.js
@@ -3,7 +3,7 @@ const Schema = mongoose.Schema;
 
 const CategorySchema = new Schema({
   alias: { type: String, required: true, unique: true },
-  name: { type: String, required: true },
+  title: { type: String, required: true },
   parent_aliases: [{ type: String }],
   image_id: { type: Schema.Types.ObjectId, ref: "Image" }
 });
