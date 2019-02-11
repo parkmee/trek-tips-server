8d9253507adaf21c0b9497cf72e6f610c696f55a
MMPark <parkmee@gmail.com>
2019-02-10 19:21:43 -0500

    edited route for yelp api

--- routes/api/yelp.js
+++ routes/api/yelp.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const yelpController = require('../../controllers/yelpController');
 
-router.route('/')
+router.route('/:id/recommendations')
   .post(yelpController.searchYelp);
 
   module.exports = router;
